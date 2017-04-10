9f141daf7e7bab4b7b7258c4c9af847faa39dcb2
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2017-04-10 11:19:50 +0200

    Add some default spec test, .fixtures.yml and .gitlab-ci.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,7 @@
+require 'rspec-puppet/spec_helper'
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'hiera'
+
+RSpec.configure do |c|
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
+end
